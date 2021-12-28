da568dd70cdcb5c7a16303a308e6565db061c150
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2021-12-29 00:59:31 +0600

    solved 2

---
+++ 2/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+/* Write a program that will reverse a string and remove all numeric characters (0-9) if it exists.
+Input:
+1791hsedalgnab
+Output:
+bangladesh
+*/
+int main()
+{
+    string s;
+    getline(cin, s);
+
+    string rev="";
+    int len=s.size();
+
+    for(int i=len; i>=0; i--){
+        rev+= s[i];
+    }
+
+    string result="";
+    for(int i=0; i<rev.size(); i++){
+        if(isalpha(rev[i])){
+           result+= rev[i];
+        }
+    }
+    cout<< result<<endl;
+    return 0;
+}
